c447bbf353ccf5c612459847531ac3f652d58b1b
giver <ryan01234keroro56789@yahoo.com.tw>
2023-05-17 23:06:13 +0800

    install.sh,README: Change repo path

--- scripts/install.sh
+++ scripts/install.sh
@@ -135,8 +135,8 @@ rvm use 3.1.2
 
 # Clone git repos
 cd "$WORKDIR"
-git clone https://github.com/TIOJ-INFOR-Online-Judge/tioj.git
-git clone https://github.com/TIOJ-INFOR-Online-Judge/tioj-judge.git
+git clone https://github.com/giver139/tioj.git
+git clone https://github.com/giver139/tioj-judge.git
 
 # Solve MariaDB incompatibility in Arch
 if [[ "$DIST" == 'Arch' ]]; then
